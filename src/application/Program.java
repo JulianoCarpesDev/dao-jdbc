952998bfeaecaaa7fe122dd0c0c48fc882eb8161
JulianoCarpes <julianocarpes.dev@gmail.com>
2023-04-06 09:51:20 -0300

    model.entites Saller and creating objects in the Program class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,23 @@
 package application;
 
+import java.util.Date;
+import java.util.Locale;
+
+import model.entites.Department;
+import model.entites.Seller;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Locale.setDefault(Locale.US);
+		
+		Department obj = new Department(1, "Books");
+		
+		Seller seller = new Seller(21, "Juliano","julianocarpes.dev@gmail.com",new Date(), 4500.0, obj);
+		
+		
+		System.out.println(seller);
 	}
 
 }
