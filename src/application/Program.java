dc1f525dc8e4aaed1312a3613f6a7183354cdb4f
JulianoCarpes <julianocarpes.dev@gmail.com>
2023-04-10 07:44:42 -0300

    implements DepartmentDaoJDBC and class program2

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,61 +1,87 @@
 package application;
 
-import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
 import model.dao.DaoFactory;
-import model.dao.SellerDao;
+import model.dao.DepartmentDao;
 import model.entites.Department;
-import model.entites.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		SellerDao sellerDao = DaoFactory.crateSellerDao(); 
-		
-		System.out.println("->->->Teste findById<-<-<-");
-		Seller seller = sellerDao.findById(1);
-		System.out.println(seller);
-		
-		System.out.println("->->->Teste findByDepartment<-<-<-");
-		Department department = new Department(3, null);
-		List<Seller>list = sellerDao.findByDepartment(department); 
-		for (Seller s : list) {
-			System.out.println(s);
+		// IMPLEMETACION DEPARTMENTDAO
+	
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+
+		System.out.println("=== TEST 1: findById =======");
+		Department dep = departmentDao.findById(1);
+		System.out.println(dep);
+
+		System.out.println("\n=== TEST 2: findAll =======");
+		List<Department> list = departmentDao.findAll();
+		for (Department d : list) {
+			System.out.println(d);
 		}
-		
-		System.out.println("->->->Teste findByAll<-<-<-");
-		
-		List<Seller>list2 = sellerDao.findAll(); 
-		for (Seller s : list2) {
-			System.out.println(s);
-		}
-		System.out.println("->->->Teste insert<-<-<-");
-		
-		Seller newSeller = new Seller(null, "Rafael","rafa.carpes@gmail.com" , new Date(), 2500.0, department);
-			
-			sellerDao.insert(newSeller);
-			
-			System.out.println("Inserted new Id = " + newSeller.getId());
-			seller = sellerDao.findById(1);
-			seller.setName("Lucas Carpes");
-			sellerDao.update(seller);
-			System.out.println("Update user Success");
-			
-			System.out.println("->->->Teste delete<-<-<-");
-			
-			System.out.print("enter user id to delete: ");
-			int id = sc.nextInt();
-			sellerDao.deleteById(id);
-			
-			System.out.println("User " + id +" has been successfully deleted");
-			
-			sc.close();
+
+		System.out.println("\n=== TEST 3: insert =======");
+		Department newDepartment = new Department(null, "Music");
+		departmentDao.insert(newDepartment);
+		System.out.println("Inserted! New id: " + newDepartment.getId());
+
+		System.out.println("\n=== TEST 4: update =======");
+		Department dep2 = departmentDao.findById(1);
+		dep2.setName("Food");
+		departmentDao.update(dep2);
+		System.out.println("Update completed");
+
+		System.out.println("\n=== TEST 5: delete =======");
+		System.out.print("Enter id for delete test: ");
+		int id = sc.nextInt();
+		departmentDao.deleteById(id);
+		System.out.println("Delete completed");
+
+		sc.close();
+
+		// IMPLETACION SELLERDAO
+		/*
+		 * SellerDao sellerDao = DaoFactory.crateSellerDao();
+		 * 
+		 * System.out.println("->->->Teste findById<-<-<-"); Seller seller =
+		 * sellerDao.findById(1); System.out.println(seller);
+		 * 
+		 * System.out.println("->->->Teste findByDepartment<-<-<-"); Department
+		 * department = new Department(3, null); List<Seller>list =
+		 * sellerDao.findByDepartment(department); for (Seller s : list) {
+		 * System.out.println(s); }
+		 * 
+		 * System.out.println("->->->Teste findByAll<-<-<-");
+		 * 
+		 * List<Seller>list2 = sellerDao.findAll(); for (Seller s : list2) {
+		 * System.out.println(s); } System.out.println("->->->Teste insert<-<-<-");
+		 * 
+		 * Seller newSeller = new Seller(null, "Rafael","rafa.carpes@gmail.com" , new
+		 * Date(), 2500.0, department);
+		 * 
+		 * sellerDao.insert(newSeller);
+		 * 
+		 * System.out.println("Inserted new Id = " + newSeller.getId()); seller =
+		 * sellerDao.findById(1); seller.setName("Lucas Carpes");
+		 * sellerDao.update(seller); System.out.println("Update user Success");
+		 * 
+		 * System.out.println("->->->Teste delete<-<-<-");
+		 * 
+		 * System.out.print("enter user id to delete: "); int id = sc.nextInt();
+		 * sellerDao.deleteById(id);
+		 * 
+		 * System.out.println("User " + id +" has been successfully deleted");
+		 * 
+		 * sc.close();
+		 */
 	}
 
 }
--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -3,12 +3,13 @@ package model.dao;
 import java.util.List;
 
 import model.entites.Department;
+import model.entites.Seller;
 
 public interface DepartmentDao {
 	
 	void insert(Department obj);
 	void update(Department obj);
-	void deleteById(Department obj);
+	void deleteById(Integer id);
 	Department findById(Integer id);
-	List<Department>findAll();
+	List<Department> findAll();
 }
